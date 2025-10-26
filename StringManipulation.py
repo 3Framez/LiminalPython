cc808a859424ad941661e1a680ff80ef3fe90189
ephraim <work4framez@gmail.com>
2025-10-26 13:02:55 -0600

    string Manipulation commit 2

--- StringManipulation.py
+++ StringManipulation.py
@@ -4,3 +4,9 @@ num = input("Enter two digit number: ") #num variable is of type string
 add = int(num[0]) + int(num[1]) #typecast the num variable to a string datatype
 print(add)
 
+
+# BMI Calculator
+height = float(input("Enter a height: ")) #typecast height from string to float
+weight = int(input("Enter a weight: ")) #typecast weight from string to int
+bmi = round(weight/(height**2)) #rounding to the nearest whole number
+print(f"Your BMI is {bmi}")
