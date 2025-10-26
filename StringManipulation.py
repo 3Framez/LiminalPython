c5faa12bdf286842459a40894fb21b779b0fa6c9
ephraim <work4framez@gmail.com>
2025-10-26 12:39:41 -0600

    string Manipulation commit

---
+++ StringManipulation.py
@@ -0,0 +1,6 @@
+num = input("Enter two digit number: ") #num variable is of type string
+
+# we subscript the num variable by seperating the number by their index position
+add = int(num[0]) + int(num[1]) #typecast the num variable to a string datatype
+print(add)
+
