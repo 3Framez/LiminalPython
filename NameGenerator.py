cf3bf0e2b70a5ff43ae1315e15711321f494f0c8
ephraim <work4framez@gmail.com>
2025-10-26 11:58:55 -0600

    generator commit

---
+++ NameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator!")
+city = input("What is the name of the city you grew up in?")
+num = input("What is your favorite number?")
+name = input("What is your name?")
+
+print(f"Your New Band name is: {city} {num} {name}")
