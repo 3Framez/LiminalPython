2628ce5878633ccf7385072f0b40183ece3b602f
ephraim <work4framez@gmail.com>
2026-02-13 01:34:51 -0700

    hangman commit final draft

--- hangman.py
+++ hangman.py
@@ -66,8 +66,8 @@ wordList = [
 import random
 from random import randint
 
-randomWordIdx = random.randint(0, len(wordList) -1)
-randomWord = wordList[randomWordIdx]
+randomWordIdx = random.randint(0, len(wordList) -1) #points to a random word's index position
+randomWord = wordList[randomWordIdx] #assigns an index position to a random word
 print(randomWord)
 
 
@@ -86,47 +86,38 @@ print(blankContainer)
 blankList = list(blankContainer)
     # print(blankList)
 
-    # letterPos = randomWord.index(randomLetter)  #doesn't work if letter is not found
-    # print(letterPos)
-# for hang in hangman:
-#     pass
+
 
 
 countHang = 0 #starts off with showing the gallow of the hangman
-counter = 0
-# step 6 loop after guessing a letter
-# while counter <= len(randomWord): #there are 7 tries counter < 7 because counter starts at 0
-# while counter <= max(len(randomWord), 7) and blankList[counter] != ['-']:
-# while counter <= min(7, len(randomWord)) and blankList[counter] != ['-']:
-# while counter <= len(randomWord) and blankList[counter] != ['-']:
+counter = 0 #starting position of the entire loop
 
-# while counter <= min(7, len(randomWord)):
-while counter <= 20: # and '-' not in blankList:
-# while blankList[counter] != '-':
+while counter <= 20:
     randomLetter = input("Enter letter: ")
 
     #step 4 assign the random letter into a blenk space if it exists in the random word
-    if randomLetter in randomWord :# and blankList[counter] != '':
+    if randomLetter in randomWord :
 
                 #step 5 get the position of the random Letter within the random word
-                letterPos = randomWord.index(randomLetter)
+                letterPos = randomWord.index(randomLetter) #the letter's index position within the word
                 # print(letterPos)
 
 
 
-                blankList[letterPos] = randomLetter
+                blankList[letterPos] = randomLetter #the letters index position within
                 print(blankList)
 
                 if blankList == list(randomWord):
                     print("You guessed the word: " + randomWord)
                     break
 
-    elif randomLetter not in randomWord and countHang < len(hangman):# and randomLetter == '':
+    #step 6 catch when the random letter is not in the random word
+    elif randomLetter not in randomWord and countHang < len(hangman):# every time this condition is met, you lose a life
 
 
-
-
-            print("You lost a life: ")
+            #gives you 6 tries
+            lifeRemaining = len(hangman) - 1 - countHang
+            print(f"You lost a life: {lifeRemaining} remaining")
             print(hangman[countHang])
             countHang += 1
             # break
