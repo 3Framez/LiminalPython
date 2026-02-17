0f7ee35b8a5b4649148fe6e4f644a7415df7d980
ephraim <work4framez@gmail.com>
2026-02-17 07:43:49 -0700

    MessageEncryption commit second draft

--- MessageEncryption.py
+++ MessageEncryption.py
@@ -24,6 +24,7 @@ def request():
     # have a list of letters
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm',
                'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # print(len(letters))
 
 
     # message = input("Type your message: ")
@@ -55,7 +56,7 @@ def request():
     # def encrypt():
     #identify the position of the recurring letters within message
         shiftedLetters = ''
-        counter = -1
+        # counter = 0
         for i in range(len(message)):
             letterPos = letters.index(message[i])  #gets the position of each of the message's character within the list of letters
             print(letterPos)
@@ -66,22 +67,94 @@ def request():
             # shiftNum = 4
             if newPosition < len(letters):
                 print(f"letter position shifted: {newPosition}")
-                print(letters[newPosition])
+                print(letters[newPosition]) #shifted result
                 shiftedLetters += letters[newPosition]
                 print(shiftedLetters)
                 #whenever the new position exceeds the length of letters, the new position goes back to the start of the list
 
             # newPosition2 = i + shiftNum
-            elif newPosition > len(letters):
-                    # for i in range(len(letters)):
-                         #go back to the first position in letters
-                        # print(newPosition)
-                    newPosition =  letterPos - shiftNum #+ counter #-> examine
-                    print(f" negative letter position shifted: {newPosition}")
-                    print(letters[newPosition])
-                    shiftedLetters += letters[newPosition]
+            elif newPosition >= len(letters):
+                # newValue = len(letters)[0]
+                # the modulo(%) remainder is the position of the new Letter when its new position > length of letters
+                #zzz (25) shifted to 4 = d (4)  || 25 + 4 % 26 = 3 (c)
+
+
+                    newLetterPos = newPosition % len(letters)  #17 % 9 || 19 % 9
+                # if newLetterPos == 0:
+                # # if newLetterPos == 0:
+                #     letters[newLetterPos] = 'a'
+                # if letters[i] == 'r':
+                #     newLetterPos == 0
+                #     newLetterPos = newPosition % len(letters)  # 17 + 9 || 19 + 9
+
+                # print(letters[newLetterPos])
+                    shiftedLetters += letters[newLetterPos]
                     print(shiftedLetters)
-                    # counter -= 3
+
+                # elif newPosition == len(letters)
+
+
+                    # newPosition =  letterPos - shiftNum #+ counter #-> examine
+                    # print(f" negative letter position shifted: {newPosition}")
+                    # print(letters[newPosition])
+                    # shiftedLetters += letters[newPosition]
+                    # print(f"Here is the decoded result: {shiftedLetters}")
+
+                # counter = 0
+            #if newPosition (shifted number + letter position within the list) is > len
+            #then the the original letter's position becomes the sarting position of the letter's list
+            # then that newPosition (26+) takes on the value of the beginning of the list of letters
+            # y (23 + 3) shifted to 3 should be b || z shifted to 3 should be c || x shifted to 3 should be a
+            # letterpos remains the same shiftNum remains the same but newPosition changes
+            # 26 (y)
+
+            # how to go back to the first position within a list if the length of list is exceeded
+
+                    # [24, 25, 26, 27, 28]
+            #                  [0,  1,  2]
+                    #if it is 26 the value would be 0, if its 27 the value would be 1
+                    #oldLetterIdx and newLetterIdx
+                    #oldLetterIdx takes on the value of newLetterIdx
+                    #newPosition = letters[0]
+                    # print(f"newPos: {newPosition - 1}")
+                    # print(f"sNum: {shiftNum}")
+                    # print(f"nL: {letters[counter]}")
+                    # # newPosition = letters[counter]
+                    # counter += 1
+
+                    #
+                    # letterRange = range(len(letters))
+                    # if x in letters > 25:
+                    #     x = 0
+                    # # for x in range(len(letters)):
+                    # #           26      -   23
+                    # newPos = newPosition - letterPos
+                    # #           3
+                    # print(f"----{newPos}")
+                    # #       letters[2]-> c || what we want
+                    # print(letters[newPos - 1])
+                    # newLetters = letters[counter + shiftNum ]
+                    # print(newLetters)
+                    # counter += 1
+                # print(letterPos)
+            #         # message[newPosition] = letters[x]
+            #         # newPosition = x
+            #     letterPos = x
+            #     print(letterPos)
+                # print(f" negative letter position shifted: {newPosition}")
+                # shiftedLetters += letters[counter]
+                # print(shiftedLetters)
+                #     counter += 1
+
+                    #go back to the first position in letters
+                    # print(newPosition)
+                    # newValue = letters[x + shiftNum]
+                #     newPosition = letters[x + shiftNum]
+                #     print(f" negative letter position shifted: {newPosition}")
+                #     print(letters[newPosition])
+                # shiftedLetters += letters[newPosition]
+                # print(shiftedLetters)
+                # counter += 1
 
             # if newPosition > len(letters):
             #     newPosition = letters[x]
