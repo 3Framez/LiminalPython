3f31057262cdd7fe581d0b17eb8e0b3d4565829c
ephraim <work4framez@gmail.com>
2026-02-18 06:44:08 -0700

    HighestBidder commit final draft

--- HighestBidder.py
+++ HighestBidder.py
@@ -1,19 +1,30 @@
 import os
 
-def clear():
-    os.system('cls')
+# def clear():
+#     os.system('cls')
 
 nameAndBid = {}
 print('Welcome to the secret auction program.')
 def request():
-    name = input('What is your name?: \n\n')
-    bid = int(input('What is your bid?: \n\n$'))
+    '''
+    DOCSTRING: it creates a dictionary with key value pairs of names and their bids
+    input: name, bid
+    :return: addBids() which asks the user if there are any additional bids
+    '''
+    name = input('What is your name?: \n')
+    bid = int(input('What is your bid?: \n$'))
     nameAndBid [name] = bid  #for every name key assign a bid value, and store in the empty defined dictionary above
+    os.system('cls') #clears once the name and bid are given
     return addBids()
 
 nameAndBid
 def addBids():
-    bid = input ("are there any other bidders? Type 'yes' or 'no'. \n\n").lower()
+    '''
+    DOCSTRING: asks the user if there are any more bids to be added
+    input: bid
+    :return: request() which asks for the name and bid of any additional bidder
+    '''
+    bid = input ("are there any other bidders? Type 'yes' or 'no'. \n").lower()
     if bid == 'yes':
         return request()
     elif bid == 'no':
@@ -24,14 +35,14 @@ def addBids():
         # getting the name of the highest bidder
         for name, height in nameAndBid.items():
             if height == maxBid: # or max(nameAndBid.values()): if baxBid hadn't been calculated
-                highestBidder = name
+                highestBidder = name #name of the bidder that has the highest bid
 
 
                 return print(f'The winner is {highestBidder} with a bid of ${maxBid}')
 
 
 
-
-request()
-clear()
-addBids()
+# function calls
+request() #the first function being called
+# clear() # better to be defined within the request function
+# addBids() #not needed to be called, because its called within the request function
